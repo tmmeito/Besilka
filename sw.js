ccff87b780978cf1535e0988dfc2251d4ffa36d6
tmmeito <68329950+tmmeito@users.noreply.github.com>
2025-08-09 09:22:41 +0200

    Improve iOS support

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('hangman-cache-v1').then(cache =>
+      cache.addAll(['./','./index.html'])
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
